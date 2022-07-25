540e523199741872c71e82f710a474b5fbfc56c5
lulinjan <516287000@qq.com>
2022-07-25 21:41:38 +0800

    发送按钮和串口联动

--- 串口上位机/main.py
+++ 串口上位机/main.py
@@ -9,17 +9,23 @@ class My_MainWindow(qw.QMainWindow, ui_test.Ui_MainWindow): #类调用
     def __init__(self):  # 初始化属性
         super().__init__()  # 调用父类属性
         self.setupUi(self)
-        # 绑定信号与曹
-        self.comboBox_btl.currentIndexChanged.connect(self.combobox_baud_cb)
+        # 绑定信号与槽
+        self.comboBox_btl.currentIndexChanged.connect(self.combobox_baud_cb)  # 波特率bobox下拉框
+        self.pushButton_sinp.clicked.connect(self.pushButton_sinp_cb)  # 发送按钮
 
-    def combobox_baud_cb(self):
+    def combobox_baud_cb(self):  # 波特率下拉框
         content = self.comboBox_btl.currentText()  # 选中的内容
         print("combox is", content)
         text = "您当前选中了%s" % content
         qw.QMessageBox.information(self, "提示", text, qw.QMessageBox.Ok | qw.QMessageBox.Cancel)  # 弹出消息框
-
         return ()  # 返回参数
 
+    def pushButton_sinp_cb(self):  # 发送按钮
+        print("发送")
+        text = self.textEdit_get.toPlainText()
+        print(" 文本内容是", text)
+        self.comboBox_uart.addItem(text)
+        
 
 if __name__ == "__main__":
     app = qw.QApplication(sys.argv)
