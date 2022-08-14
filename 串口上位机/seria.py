a59ce496598e76314a21c173e723969892c38b52
林健 <516287000@qq.com>
2022-08-15 00:20:58 +0800

    修改seria.py测试串口基本程序

--- 串口上位机/seria.py
+++ 串口上位机/seria.py
@@ -11,6 +11,7 @@
 import sys
 import serial
 import threading
+from time import sleep
 
 
 class Uart(object):
@@ -19,15 +20,46 @@ class Uart(object):
         try:
             self.serial = serial.Serial(port, baud)
             print("打开串口成功")
-
-
-
         except:
             print("打开串口失败")
             self.err = -1
+
+    def uar_recv_thread(self):
+        print("运行uart_recv_thread")
+        while (True):
+            try:
+                recv_data_raw = self.serial.readline()
+                data = "接收----PC" + recv_data_raw.decode()
+                print(data)
+            except:
+                print("接收数据错误!")
+                break
+
+    def start_recv_thread(self):  # 接收线程
+        thread = threading.Thread(target=self.uar_recv_thread(), daemon=True)
+        thread.start()
+
+    def send_uart_data(self, data):  # 发送数据
+        self.serial.write(data.encode())
+
+    def uart_close(self):  # 关闭端口
+        self.serial.close()
 
 
 if __name__ == '__main__':
     myuart = Uart("com2", 9600)
     if (0 == myuart.err):
-        print("打开串口")
+        print("打开串口成功")
+        # 如果打开串口成功,启动接收线程,准备实时的接收数据
+    myuart.start_recv_thread()
+    while (True):
+        input_data = input("输入要发送的数据...")
+        if (input_data == "quit"):
+            # 退出
+            myuart.uart_close()
+            break
+        else:
+            # 发送数据给设备
+            myuart.send_uart_data(str(input_data))
+        sleep(0.01)
+    print("退出")
---
+++ 串口上位机/test.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time : 2022/8/14
+@Author : Administrator
+@File : test
+@Description : 
+"""
+import sys
+import serial
+import threading
+
+if __name__ == '__main__':
+    print_hi('Python'
